20773bd7b85f61678dda30af073a3ee7d7f000f0
hmc1011 <hmc10111994@gmail.com>
2021-04-01 20:43:08 +0700

    setup the blog table

---
+++ app/code/HoangCong/Blog/Api/PostRepositoryInterface.php
@@ -0,0 +1,33 @@
+<?php
+namespace HoangCong\Blog\Api;
+
+/**
+ * Interface PostRepository
+ * @package HoangCong\Blog\Api
+ */
+interface  PostRepositoryInterface
+{
+    /**
+     * @param int $id
+     * @return \ViMagento\CustomApi\Api\Data\CustomInterface
+     */
+    public function getById($id);
+
+    /**
+     * @param \ViMagento\CustomApi\Api\Data\CustomInterface $vimagento
+     * @return \ViMagento\CustomApi\Api\Data\CustomInterface
+     */
+    public function save(CustomInterface $vimagento);
+
+    /**
+     * @param int $id
+     * @return bool
+     */
+    public function deleteById($id);
+
+    /**
+     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+     * @return \ViMagento\CustomApi\Api\Data\CustomSearchResultInterface
+     */
+    public function getList(SearchCriteriaInterface $searchCriteria);
+}
