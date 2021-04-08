e80918c40ab0cecb922f5c738238a8b2abf02401
hmc1011 <hmc10111994@gmail.com>
2021-04-08 17:52:04 +0700

    add form ui

---
+++ app/code/HoangCong/Blog/Model/DataProvider.php
@@ -0,0 +1,37 @@
+<?php
+namespace HoangCong\Blog\Model;
+ 
+use HoangCong\Blog\Model\ResourceModel\Post\CollectionFactory;
+ 
+class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param CollectionFactory $postCollectionFactory
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $postCollectionFactory,
+        array $meta = [],
+        array $data = []
+    ) {
+        $this->collection = $postCollectionFactory->create();
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+    }
+ 
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    // public function getData()
+    // {
+    //     return ['name'=>'cong'];
+    // }
+}
